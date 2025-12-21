2aa61f4241f74b70326402ba3dc2b410134b6e4d
Craig Sampson <craigsjr15@gmail.com>
2025-12-21 18:43:21 -0500

    hero updated

--- app/components/ui/sections/Hero.tsx
+++ app/components/ui/sections/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section
       id="home"
-      className=" relative bg-secondary/25 pt-[20px] xl:pt-[150px] h-fit overflow-visible pb-6 flex items-center w-full justify-center"
+      className=" relative bg-secondary/25 pt-20px xl:pt-[150px] h-fit overflow-visible pb-6 flex items-center w-full justify-center"
     >
       <Image
         src="/bjj-staten-island.svg"
@@ -20,11 +20,11 @@ export default function Hero() {
       <div className="relative flex flex-col items-start justify-between gap-5 w-fit max-w-2xl xl:max-w-5xl 2xl:max-w-7xl py-4 px-4 xl:px-40">
         <div className="flex items-center justify-center gap-4">
           <h1 className="border-l-2 md:border-l-3 pl-5 border-primary">
-            Staten Island&apos;s Premier Mixed Martial Arts Program
+            Pierro MMA - Staten Island&apos;s Premier Mixed Martial Arts Program
           </h1>
         </div>
         <div>
-          <h2 className="!font-oswald !text-xl lg:!text-2xl 2xl:!text-4xl !font-medium !leading-normal !text-white max-w-4xl">
+          <h2 className="font-oswald! text-xl! lg:text-2xl! 2xl:text-4xl! font-medium! leading-normal! text-white! max-w-4xl">
             World class coaching combined with state-of-the-art facilities
             provide the perfect environment to master MMA, BJJ, and more in
             Staten Island.
